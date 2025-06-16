84e14f8253e030ac95a4cfd7d0adca8c11dd9b7c
matias <matia@MATIPC>
2025-06-15 22:08:01 -0500

    Agregar archivos de proyecto.

---
+++ NotasMatiasMonteros/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NotasMatiasMonteros
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
